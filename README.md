52b1d70be2f9ed77b599a4e021f90af0e7ac50a5
aboynejames <james@aboynejames.co.uk>
2017-11-21 10:30:01 +0000

    command line shell script

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# guardiancoll
-a guardian collaboration node on the LKN network
+# guardian
+-----------
+Guardian - the LKN protocol
+===========================
+
+Guardians play the same role as miners do in bitcoin, ethereum etc but for science. In the LKN they co-ordination a consensus mechanism that keep all science honest and truthful.
+
+This will be a new peer to peer consensus mechanism that will be researched and tested before going live.  Initial community thinking envisions a modular software stack secured by a knowledge scripting language.
+
+
+Command line shell
+=================
+
+A node.js command-line has been implemented:
+
+Install
+-------
+
+
+
+
+Introduction Slides and documentation
+=====================================
+
+Knowledge Scripting Language
+----------------------------
+A new scripting language allowing modules to securely interact with protocol.
+
+
+Peer to Peer Network
+---------------------
+Existing peer to peer network libraries bing tested
+https://protocol.ai/projects/#Libp2p
+https://github.com/maidsafe/crust
+
+
+Computational Reference Layers
+------------------------------
+All science must be in a computational form entering the LKN.  This raises the capability from data model thinking to a data types that can be used by other software intantly across the network.
+
+
+Consensus Science Cycles
+------------------------
+A range of new consensus mechanisms will be brought forward that are agreed by the community to adjudicate on the truthfulness and confidence in claims made by the science in the network.  This will be the most challenging part of the LKN project.
+
+
+Issuing & Securing Knowledge Tokens
+-----------------------------------
+A new knowledge currency to value science.  This will contain an immutable proof of evidence ledger.
---
+++ src/index.js
@@ -0,0 +1,117 @@
+#!/usr/bin/env node
+'use strict';
+const program = require('commander');
+const inquirer = require('inquirer');
+const getIP = require('external-ip')();
+
+program
+    .version('0.0.1')
+    .usage('[options] <LKN>')
+    .description('Living Knowledge Network Protocol')
+    .option('-a, --address [addr]', 'Node identity')
+    .option('-p, --port [port]', 'Port to connect')
+    .parse(process.argv);
+
+if(!program.args.length) {
+  program.help();
+}
+else
+{
+console.log('LKN: ' + program.args);
+console.log('Address: ' + program.address);
+console.log('Port: ' + program.port);
+//console.log(program);
+    var questions = [
+      {
+        type: 'input',
+        name: 'incommand',
+        message: '>>>'
+      }
+    ];
+
+    ask();
+};
+
+function ask() {
+
+  inquirer.prompt(questions).then(function (answers) {
+    if(answers.incommand == 'exit')
+    {
+console.log('original exit');
+      // need to exit the DHT
+      process.exit(1);
+    }
+    else if(answers.incommand == 'lkn')
+    {
+console.log('connecting to Living Knowledge Network');
+      lknConnect(program.address, program.port);
+    }
+    else if(answers.incommand == 'consensus')
+    {
+console.log('starting consensus science cycle');
+      consensus();
+    }
+    else if(answers.incommand != 'lkn' || answers.incommand != 'consensus' || answers.incommand != 'exit' )
+    {
+console.log('Command not recognised, please use help');
+      ask();
+    }
+    else
+    {
+console.log('Stopped:', output.join(', '));
+      process.exit(1);
+    }
+  });
+};
+
+function lknConnect (addressIn, portIn) {
+console.log(addressIn);
+console.log(portIn);
+  var portnumber = parseInt(portIn);
+  ask();
+
+};
+
+function consensus() {
+
+  var questionseed = [
+    {
+      type: 'input',
+      name: 'scicycleid',
+      message: 'Enter ID of Science Cycle:'
+    },
+    {
+      type: 'input',
+      name: 'guardsig',
+      message: 'Guaridan signature:'
+    },
+    {
+      type: 'input',
+      name: 'knowledgescript',
+      message: 'Knowledge Script:'
+    }
+
+  ];
+  inquirer.prompt(questionseed).then(function (answerseed) {
+
+    var output = [];
+    output.push(answerseed.scicycleid);
+    output.push(answerseed.guardsig);
+    output.push(answerseed.knowledgescript);
+console.log('Consensus message formed');
+console.log(output);
+    if(answerseed.knowledgescript)
+    {
+      //  now send message to LKN
+      lknMessage(output);
+
+    }
+  });
+
+};
+
+function lknMessage(seedIn) {
+console.log('form Knowledge Scripting Language for Consensus Science Cycle');
+  ask();
+
+};
